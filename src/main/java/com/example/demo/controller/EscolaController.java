05dab0a3c1d7a2b37378dec095cd55cd5f437c41
anderson <anderson@home>
2018-11-19 11:43:50 -0300

    .

--- src/main/java/com/example/demo/controller/EscolaController.java
+++ src/main/java/com/example/demo/controller/EscolaController.java
@@ -1,13 +1,24 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.services.ServiceTest;
 
 @Controller
 public class EscolaController {
 	
+	@Autowired
+	ServiceTest serviceTest;
+	
 	@GetMapping(value="/")
-	public String index(){
+	public String index(HttpSession session, Model model){
+		session.setAttribute("pemissions", "All Permissions");
+		model.addAttribute("ServiceTest", serviceTest);
 		return "index/index";
 	}
 	
